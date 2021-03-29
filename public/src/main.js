6fee1a98915e543056413e03874fc62bc4c00ab2
Juddroid <raccoon@Juddroidui-MacBookPro.local>
2021-03-30 01:17:15 +0900

    feat: tokenizerList 개발

--- public/src/main.js
+++ public/src/main.js
@@ -1,82 +1,69 @@
 import '../scss/style.scss';
 import MainView from '../view/view';
+import packageJson from '../../package.json';
+
+const packageJsonData = JSON.stringify(packageJson);
+console.log(packageJsonData);
+
 const mainView = new MainView();
 
-// import dataJson from "../data/data.js";
-const data = `const data = "[1]";`;
 const dataJson = [
   '1a3',
   [null, false, ['11', [112233], { easy: ['hello', { a: 'a' }, 'world'] }, 112], 55, '99'],
   { a: 'str', b: [912, [5656, 33], { key: 'innervalue', newkeys: [1, 2, 3, 4, 5] }] },
   true,
 ];
-const jsonString = JSON.stringify(dataJson);
-console.log(dataJson);
-console.log(jsonString);
 
-const json = {
-  name: 'fe-w8-json-parser',
-  version: '1.0.0',
-  description: '',
-  main: 'server.js',
-  scripts: {
-    build: 'webpack',
-    dev: 'webpack serve',
-    start: 'nodemon server.js',
-  },
-};
-const jsonArr = [];
-for (let data in json) {
-  jsonArr.push(data);
-  jsonArr.push(json[data]);
-}
-console.log(jsonArr);
+const jsonData = JSON.parse(JSON.stringify(dataJson));
 
-// identifier : 식별하기 위한 이름- > 변수명 dataArr,
-// keyword : 미리 지정한 예약어 -> ex) const, if, for, class, while, forEach..
-// separator : 글자를 구분하는 문자 -> 구분자 ex) {, }, [, ], ,
-// operator : 연산을 위한 심볼 -> ex) + - ,%, =, ;,
-// literal : 숫자, 논리, 문자 -> true, 3.14, &&,
-// comment : 줄 또는 블록 코멘터리 -> 주석..
-
-const parse = {
-  identifier: 'luke',
-  keyword: '',
-  seperator: '',
-  operator: '',
-  literal: '',
-  comment: '',
+const parserObject = {
+  identifier: [],
+  keyword: [],
+  seperator: [],
+  operator: [],
+  literal: [],
+  comment: [],
 };
 
-function tokenParser(data) {
-  return isKeyword(data.split(' '));
-}
-console.log(tokenParser(data));
+const tokenList = [];
 
-function isKeyword(data) {
-  // 더 많을 수 있음
-  const keywordArr = [];
-  const keyword = ['const', 'let', 'var', 'if', 'for', 'class', 'while'];
+function getTokenList(data) {
   data.forEach((el) => {
-    if (keyword.includes(el)) {
-      keywordArr.push(el);
+    console.log(el);
+    console.log(typeof el);
+    switch (typeof el) {
+      case 'string':
+      case 'number':
+      case 'boolean':
+      case 'null':
+        pushToken(el);
+        break;
+      case 'object':
+        checkObject(el);
+        break;
+      default:
+        break;
     }
   });
-
-  parse.keyword = keywordArr;
-  return isIdentifier(data);
+  return tokenList;
 }
 
-function isIdentifier(data) {
-  const identifierArr = [];
-  const identifier = ['+', '-', '='];
-  data.forEach((el, idx) => {
-    if (identifier.includes(el)) {
-      identifierArr.push(data[idx - 1]);
-    }
-  });
-  parse.identifier = identifierArr;
-  console.log(parse);
+function pushToken(data) {
+  tokenList.push(data);
 }
 
-function isSeperator() {}
+function checkObject(data) {
+  if (data === null) return pushToken(data);
+  if (Array.isArray(data)) {
+    return getTokenList(data);
+  } else {
+    return getObjectToken(data);
+  }
+}
+
+function getObjectToken(data) {
+  const objKey = Object.keys(data);
+  const objValue = Object.values(data);
+  getTokenList(objKey);
+  getTokenList(objValue);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: ["@babel/polyfill", "./public/src/main.js"],
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: ['@babel/polyfill', './public/src/main.js'],
+  devtool: 'inline-source-map',
   output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "http://localhost:3000",
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'http://localhost:3000',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new CleanWebpackPlugin(),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname + "/dist"),
-    index: "index.html",
+    contentBase: path.resolve(__dirname + '/dist'),
+    index: 'index.html',
     port: 3000,
     writeToDisk: true,
     hot: true,
