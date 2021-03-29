77b300eec43213c2c8fbb9467cd30e30cab6aad7
Juddroid <raccoon@Juddroidui-MacBookPro.local>
2021-03-29 22:50:26 +0900

    chore: 끝내지못한 일

--- public/view/view.js
+++ public/view/view.js
@@ -1,8 +1,5 @@
 import { _ } from "../src/util.js";
 const $root = _.$("#root");
-export default mainView() {
-//     return 
-//     `<div class="root__body-container">
-//     <div class=>
-//     </div>`;
+export default function mainView() {
+    
 }
