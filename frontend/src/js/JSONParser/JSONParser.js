ebd2e95a45182c276f3586c2e8118d45708b9be5
Hoyoung Son <xzxking17@gmail.com>
2021-03-30 16:57:31 +0900

    Feat: Tokenizer 기능 추가

--- frontend/src/js/JSONParser/JSONParser.js
+++ frontend/src/js/JSONParser/JSONParser.js
@@ -17,29 +17,16 @@ class JSONParser {
 
     init = () => {
         this.setAnalysisBtnClickEvent(this.analysisBtn);
-        const testData =
-            '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
-        const test = this.tokenizer.getTokenType(testData);
-        console.log(test);
     };
 
     setAnalysisBtnClickEvent = (analysisBtn) =>
         _.ON(analysisBtn, 'click', (e) => this.analysisBtnClickEventHandler(e));
-    analysisBtnClickEventHandler = ({ target }) => {
-        let result = `{`;
-        const tempData = '[1]';
-        tempData.split('').forEach((char) => {});
-
-        /*
-        {
-            "type": "array",
-            "child": [
-                {
-                    "value": 1,
-                    "type": "number"
-                },
-            ]
-        */
+    analysisBtnClickEventHandler = () => {
+        const testData =
+            '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
+        this.tokenizer.getTokenType(testData);
+        console.log(this.tokenizer.stack);
+        this.resultTextArea.value = this.tokenizer.stack.join(' ');
     };
 }
 
--- frontend/src/js/JSONParser/partial/Tokenizer.js
+++ frontend/src/js/JSONParser/partial/Tokenizer.js
@@ -1,57 +1,32 @@
 // Tokenizer : token 으로 자르기 -> 잘라진 토큰 정보가 모인 배열을 반환할 수 있음
 class Tokenizer {
     constructor() {
-        // https://lucas.codesquad.kr/main/course/마스터즈-FE-클래스/8주차-JSON-Parser/AST생성과-분석
         this.tokenType = {
-            SEPARATOR: /\{|\}|\[|\]|\,|(\s?:\s?)|(\s+)/,
-            OPERATOR: /\+|\<|\>|\=|\*|\/|\-/,
+            SEPARATOR: /\{|\}|\[|\]|\,|:|\s/,
             LITERAL: /null|undefined|(true|false)|(-?[0-9]+(?:\.[0-9]+)?)|("(?:[^"\\]*|\\")*")/,
         };
         this.stack = [];
-        this.stack2 = [];
-    }
+    };
     getTokenType = (charData) => {
-        const { SEPARATOR, OPERATOR, LITERAL } = this.tokenType;
+        const { SEPARATOR, LITERAL } = this.tokenType;
         let charDataTemp = charData;
-        let cnt = 0;
 
-        while (!charDataTemp.length) {
-            if (SEPARATOR.test(charDataTemp)) {
-            } else {
-                if (LITERAL.test(charDataTemp)) {
-                    const currentStr = charDataTemp.match(LITERAL)[0];
-                    const currentStrLength = currentStr.length;
-                    charDataTemp = charDataTemp.replace(currentStr, '');
-                    console.log(currentStr);
-                    console.log(charDataTemp);
-                    this.stack2.push(charDataTemp.match(LITERAL)[0]);
-                }
+        while (charDataTemp.length > 0) {
+            if (charDataTemp[0] === " ") {
+                charDataTemp = charDataTemp.slice(1);
+                continue;
             }
-            cnt++;
+
+            if (SEPARATOR.test(charDataTemp) && SEPARATOR.test(charDataTemp[0])) {
+                const currentStr = charDataTemp.match(SEPARATOR)[0];
+                charDataTemp = charDataTemp.replace(currentStr, '');
+                this.stack.push(currentStr);
+            } else {
+                const currentStr = charDataTemp.match(LITERAL)[0];
+                charDataTemp = charDataTemp.replace(currentStr, '');
+                this.stack.push(currentStr);
+            }
         }
-
-        // while (!charDataTemp.length) {
-
-        //     const currentChar = charDataTemp[cnt];
-        //     if (SEPARATOR.test(currentChar)) {
-        //         charDataTemp = charDataTemp.replace(currentChar, '');
-        //         this.stack.push(currentChar);
-        //     } else {
-        //         if (LITERAL.test(charDataTemp)) {
-        //             const currentStr = charDataTemp.match(LITERAL)[0];
-        //             const currentStrLength = currentStr.length;
-        //             charDataTemp = charDataTemp.replace(currentStr, '');
-        //             console.log(currentStr);
-        //             console.log(charDataTemp);
-        //             this.stack2.push(charDataTemp.match(LITERAL)[0]);
-        //         }
-        //     }
-        //     cnt++;
-        // }
-        // console.log(charDataTemp);
-
-        // console.log('stack1', this.stack);
-        // console.log(this.stack2);
     };
 }
 
