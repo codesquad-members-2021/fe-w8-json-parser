361d64d79c7f56e69f0a106bd315989a098d798f
Hongbi <dyongdi@gmail.com>
2021-03-30 15:33:14 +0900

    Refactor: change tokenizer.js from OOP to FP

--- tokenizer.js
+++ tokenizer.js
@@ -1,34 +1,19 @@
-class Tokenizer {
-    constructor(str){
-        this.queue = [];
-        this.seperator = ["[", "]", "{", "}", ",", ":"];
-        this.tokenize(str);
-    }
+const tokenize = (str, temp = [], queue = []) => {
+    const seperator = ["[", "]", "{", "}", ",", ":"];
+    if (!str.length) return queue;
+    
+    if(str[0] === ' ' && isQuoteClosed(temp))
+        return tokenize(str.substring(1), temp, queue);
 
-    tokenize(str, temp = []){
-        if (!str.length) return
-        
-        if(str[0] === ' ' && this.isQuoteClosed(temp))
-            return this.tokenize(str.substring(1), temp);
-
-        if (this.seperator.includes(str[0]) && this.isQuoteClosed(temp)) {
-            if (temp.length > 0) this.queue.push(temp.join('').trim());
-            this.queue.push(str[0]);
-            return this.tokenize(str.substring(1), []);
-        }
-
-        else {
-            return this.tokenize(str.substring(1), [...temp, str[0]]);
-        }
-    }
-
-    isQuoteClosed(tokenArr){
-        return tokenArr.filter(e => e === `"`).length !== 1;
-    }
-
-    getToken(){
-        return this.queue;
+    if (seperator.includes(str[0]) && isQuoteClosed(temp)) {
+        if (temp.length > 0) queue.push(temp.join('').trim());
+        queue.push(str[0]);
+        return tokenize(str.substring(1), [], queue);
+    } else {
+        return tokenize(str.substring(1), [...temp, str[0]], queue);
     }
 }
 
-export { Tokenizer }
+const isQuoteClosed = tokenArr => tokenArr.filter(e => e === `"`).length !== 1;
+
+export { tokenize }
