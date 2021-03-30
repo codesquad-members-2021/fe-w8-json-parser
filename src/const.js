d98a0d994298c460321b9f099b7c11583e3f3c71
cchoongh <cndgh49@naver.com>
2021-03-30 11:06:52 +0900

    Add const.js

---
+++ src/const.js
@@ -0,0 +1,14 @@
+export const Type = {
+  LBRAKET: 0,
+  RBRAKET: 1,
+  LBRACE: 2,
+  RBRACE: 3,
+  COLON: 4,
+
+  STRING: 11,
+  BOOLEAN: 12,
+  NUMBER: 13,
+  OBJECT: 14,
+  ARRAY: 15,
+  NULL: 16,
+}
