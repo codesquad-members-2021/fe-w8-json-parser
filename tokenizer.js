4c5e5bb232ff657671ffd215f6d13ed9f818c19d
Hongbi <dyongdi@gmail.com>
2021-03-30 15:18:23 +0900

    Fix: fix space issue
    문자열이 아닌 공백이 token으로 추가되는 문제를 고쳤다.

--- tokenizer.js
+++ tokenizer.js
@@ -8,7 +8,8 @@ class Tokenizer {
     tokenize(str, temp = []){
         if (!str.length) return
         
-        if (this.isQuoteClosed(temp)) temp = temp.filter(e => e !== ' ');
+        if(str[0] === ' ' && this.isQuoteClosed(temp))
+            return this.tokenize(str.substring(1), temp);
 
         if (this.seperator.includes(str[0]) && this.isQuoteClosed(temp)) {
             if (temp.length > 0) this.queue.push(temp.join('').trim());
@@ -16,7 +17,9 @@ class Tokenizer {
             return this.tokenize(str.substring(1), []);
         }
 
-        else return this.tokenize(str.substring(1), [...temp, str[0]]);
+        else {
+            return this.tokenize(str.substring(1), [...temp, str[0]]);
+        }
     }
 
     isQuoteClosed(tokenArr){
@@ -28,10 +31,4 @@ class Tokenizer {
     }
 }
 
-const test1 = `["a","b",["c","d"],1,"]["]`
-
-const test2 = `["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"]]`
-
-const test3 = '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]'
-
-console.log(new Tokenizer(test3).getToken())
+export { Tokenizer }
