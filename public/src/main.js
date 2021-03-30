af979e3de7a14187c7337b8e1433d9b152e68bf2
reminderclock <reminderclock@naver.com>
2021-03-30 18:01:51 +0900

    feat: Tokenizer 수정

--- public/src/main.js
+++ public/src/main.js
@@ -1,63 +1,51 @@
 import '../scss/style.scss';
 import MainView from '../view/view';
-import packageJson from '../../package.json';
-import { inputTitle } from './const';
-
-const packageJsonData = JSON.stringify(packageJson);
-console.log(packageJsonData);
 
 const mainView = new MainView();
 
-const inputData = '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
-const tokenList = [];
-// console.log("" === '');
-function getTokenList(data) {
-  let splitData = data.split('');
-  splitData.forEach((el, idx) => {
-    switch (el) {
-      case '"':
-        getTokenString(el, idx);
-        break;
-      case 'n':
-        getTokenNull(el);
-      case 'f':
-      case 't':
-        getTokenBoolean(el);
-      case '[':
-        getTokenArr(el);
-      case '{':
-        getTokenObj(el);
-      default:
-        break;
+const inputData = '["1a3",[null,false,["11",[112233],{"easy" : ["hel]lo", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
+const saperatorArr= ['[',']',',','{','}',':'];
+const tokenArr = [];
+const stack = [];
+function getToken(inputData) {
+  for(let i=0; i<inputData.length; i++) {
+    if(isSaperator(inputData[i])) {
+      pushToken(inputData[i])
+    }
+
+    else if(isSaperator(inputData[i-1]) && isNotFisrt(i)) {
+      pushToken(inputData[i])
+    }
+
+    else if(!isSaperator(inputData[i-1]) && isNotFisrt(i)) {
+      makeString(inputData[i]);
     }
   }
-  );
+  return tokenArr;
+}
+function pushToken(data) {
+  tokenArr.push(data);
 }
 
-function getTokenArr(el) {
-  console.log(el);
-}
-function getTokenBoolean(el) {
-  console.log(el);
-}
-function getTokenObj(el) {
-  console.log(el);
-}
-function getTokenNull(el) {
-  console.log(el);
+function makeString(data) {
+return tokenArr[tokenArr.length-1]+=data;
 }
 
-
-
-function getTokenString(data, idx) {
-  debugger;
-  if(data === '"') return;
-  let tokenString = '';
-  while(data !== '"') {
-    tokenString += data;
-    idx++;
-  }
-  return console.log(tokenString);
+function isSaperator(data) {
+return saperatorArr.includes(data)
 }
-getTokenList(inputData);
+
+function isNotFisrt(idx) {
+return idx>=1
+}
+
+function isQuote(data) {
+return data === '"'
+}
+
+function isFullSatck() {
+return stack.length !==0
+}
+getToken(inputData.split(''));
+
 
