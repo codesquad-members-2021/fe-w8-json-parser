0171bcbff38589ddd65e18d3127174eac2233b83
reminderclock <reminderclock@naver.com>
2021-03-30 18:49:10 +0900

    feat: tokenizer 구현

--- public/src/main.js
+++ public/src/main.js
@@ -3,24 +3,35 @@ import MainView from '../view/view';
 
 const mainView = new MainView();
 
-const inputData = '["1a3",[null,false,["11",[112233],{"easy" : ["hel]lo", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
+const inputData = '["1a3",["she\'s gone", null,false,["11",[112233],{"easy" : ["hel]lo", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
 const saperatorArr= ['[',']',',','{','}',':'];
 const tokenArr = [];
 const stack = [];
 function getToken(inputData) {
   for(let i=0; i<inputData.length; i++) {
-    if(isSaperator(inputData[i])) {
+    if(isQuote(inputData[i]) && !isFullSatck()) {
+      stack.push(inputData[i])
+      pushToken(inputData[i])
+      continue
+    }
+    else if (isQuote(inputData[i]) && isFullSatck()) {
+      stack.pop();
+    }
+
+    if(isSaperator(inputData[i]) && !isFullSatck()) {
       pushToken(inputData[i])
     }
 
-    else if(isSaperator(inputData[i-1]) && isNotFisrt(i)) {
+    else if(isSaperator(inputData[i-1]) && isNotFisrt(i) && !isFullSatck()) {
       pushToken(inputData[i])
     }
 
-    else if(!isSaperator(inputData[i-1]) && isNotFisrt(i)) {
+    else {
       makeString(inputData[i]);
     }
   }
+  const tokenList = tokenArr.map(el => el.trim());
+  console.log(tokenList);
   return tokenArr;
 }
 function pushToken(data) {
