1fd93c73452397a560344c91b364596cb4f4c63f
cchoongh <cndgh49@naver.com>
2021-03-31 16:28:24 +0900

    Modify lexer to return value having primitive

--- src/lexer.js
+++ src/lexer.js
@@ -5,26 +5,22 @@ export function lexer(tokens) {
   const resultTokens = [];
 
   tokens.forEach(token => {
-    const type = getType(token);
-    resultTokens.push({
-      type,
-      value: token
-    });
+    resultTokens.push(getLexerToken(token));
   });
 
   return resultTokens;
 }
 
-export function getType(token) {
-  if (token === '[') return Type.LBRAKET;
-  if (token === ']') return Type.RBRAKET;
-  if (token === '{') return Type.LBRACE;
-  if (token === '}') return Type.RBRACE;
-  if (token === ':') return Type.COLON;
-  if (token[0] === '\"' && token[token.length - 1] === '\"') return Type.STRING;
-  if (token === 'true' || token === 'false') return Type.BOOLEAN;
-  if (token === 'null') return Type.NULL;
-  if (token !== '' && Number(token) !== NaN) return Type.NUMBER;
+export function getLexerToken(token) {
+  if (token === '[') return { type: Type.LBRAKET, value: token };
+  if (token === ']') return { type: Type.RBRAKET, value: token };
+  if (token === '{') return { type: Type.LBRACE, value: token };
+  if (token === '}') return { type: Type.RBRACE, value: token };
+  if (token === ':') return { type: Type.COLON, value: token };
+  if (token[0] === '\"' && token[token.length - 1] === '\"') return { type: Type.STRING, value: token };
+  if (token === 'true' || token === 'false') return { type: Type.BOOLEAN, value: Boolean(token) };
+  if (token === 'null') return { type: Type.NULL, value: null };
+  if (token !== '' && Number(token) !== NaN) return { type: Type.NUMBER, value: Number(token) };
 
   throw new Error(`Invalid argument, ${token}`);
 }
--- src/test/lexer.js
+++ src/test/lexer.js
@@ -1,20 +1,6 @@
 import { Type } from '../const.js';
 import * as Lexer from '../lexer.js';
 
-function test_getDataType() {
-  console.log('run test_getDataType()');
-  console.log('boolean test1: ', Lexer.getType('true') === Type.BOOLEAN);
-  console.log('boolean test2: ', Lexer.getType('false') === Type.BOOLEAN);
-  console.log('number test1: ', Lexer.getType('1.1') === Type.NUMBER);
-  console.log('number test2: ', Lexer.getType('100') === Type.NUMBER);
-  console.log('number test3: ', Lexer.getType('-1') === Type.NUMBER);
-  console.log('string test1: ', Lexer.getType('\"123\"') === Type.STRING);
-  console.log('string test2: ', Lexer.getType('\"abc\"') === Type.STRING);
-  console.log('array test1: ', Lexer.getType('[') === Type.ARRAY);
-  console.log('object test1: ', Lexer.getType('{') === Type.OBJECT);
-  console.log('null test1: ', Lexer.getType('null') === Type.OBJECT);
-}
-
 function test_lexer() {
   console.log(`run test_lexer()`);
   const tcs = [
@@ -28,5 +14,5 @@ function test_lexer() {
   });
 }
 
-test_getDataType();
-test_lexer();
+// test_getDataType();
+// test_lexer();
