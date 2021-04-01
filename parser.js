af115e01835737459e4d6ee2a3fe1a7fc431c987
Hongbi <dyongdi@gmail.com>
2021-04-01 16:04:38 +0900

    Refactor: refactor parser.js
    - makePropertyTemplate 함수 분리
    - 함수를 한 줄로 줄임
    -

--- parser.js
+++ parser.js
@@ -1,59 +1,37 @@
-const parse = (lexedArr, parentNode = {child:[]}, isObject = false, ObjIndex = 0) => {
-  if(!lexedArr.length) return parentNode
+const parse = (lexedArr, parentNode = {child:[]}, isObject = false, objIndex = 0) => {
+  if(!lexedArr.length) return parentNode.child;
   const {type, value} = lexedArr.shift()
   switch(type) {
     case 'separator':
       if(value === "[")
-        addNode(parse(lexedArr, makeArrayTemplate()), parentNode, isObject, ObjIndex)
+        addNode(parse(lexedArr, makeArrayTemplate()), parentNode, isObject, objIndex)
       if(value === "{")
-        addNode(parse(lexedArr, makeObjectTemplate(), "propKey"), parentNode, isObject, ObjIndex)
+        addNode(parse(lexedArr, makeObjectTemplate(), "propKey"), parentNode, isObject, objIndex)
       if(value === "]" || value === "}")
         return parentNode
       if(value === ":")
-        return parse(lexedArr, parentNode, "propValue", ObjIndex)
-      if(value === ",")
-        if(isObject === "propValue") {
-          parentNode.child.push({
-            "value": {
-              "propKey": {},
-              "propValue": {}
-            },
-            "type": "objectProperty"
-          })
-          return parse(lexedArr, parentNode, "propKey", ++ObjIndex)
-        }
+        return parse(lexedArr, parentNode, "propValue", objIndex)
+      if(value === "," && isObject === "propValue") {
+        parentNode.child.push(makePropertyTemplate())
+        return parse(lexedArr, parentNode, "propKey", ++objIndex)
+      }
       break;
     default :
-      addNode({type, value}, parentNode, isObject, ObjIndex)
+      addNode({type, value}, parentNode, isObject, objIndex)
   }
-  return parse(lexedArr, parentNode, isObject, ObjIndex)
+  return parse(lexedArr, parentNode, isObject, objIndex)
 }
 
-const addNode = (node, parentNode, isObject, ObjIndex) => {
-  if (isObject === "propKey") parentNode.child[ObjIndex].value.propKey = node
-  else if (isObject === "propValue") parentNode.child[ObjIndex].value.propValue = node
+const addNode = (node, parentNode, isObject, objIndex) => {
+  if (isObject === "propKey") parentNode.child[objIndex].value.propKey = node
+  else if (isObject === "propValue") parentNode.child[objIndex].value.propValue = node
   else parentNode.child.push(node)
 }
 
-const makeArrayTemplate = () => {
-  return {
-    type: "array",
-    child: [],
-    value: "arrayObject"
-  }
-}
+const makeArrayTemplate = () => ({type: "array", child: [],value: "arrayObject"})
 
-const makeObjectTemplate = () => {
-  return {
-    "type": "object",
-    "child": [{
-      "value": {
-        "propKey": {},
-        "propValue": {}
-      },
-      "type": "objectProperty"
-    }]
-  }
-}
+const makeObjectTemplate = () => ({type: "object", child: [makePropertyTemplate()]})
+
+const makePropertyTemplate = () => ({value: {propKey: {}, propValue: {}}, type: "objectProperty"})
 
 export { parse }
