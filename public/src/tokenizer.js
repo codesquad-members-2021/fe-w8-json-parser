c07e96d613d63a36e7cc4d6f1829b1416f9508e1
Juddroid <raccoon@Juddroidui-MacBookPro.local>
2021-04-01 16:28:51 +0900

    feat: Create Tokenizer file

---
+++ public/src/tokenizer.js
@@ -0,0 +1,55 @@
+export default class Tokenizer {
+  constructor(inputData) {
+    this.inputData = inputData;
+    this.tokenArr = [];
+    this.stack = [];
+    this.getToken();
+  }
+  getToken() {
+    for (let i = 0; i < this.inputData.length; i++) {
+      if (this.isQuote(this.inputData[i]) && !this.isFullSatck()) {
+        this.stack.push(this.inputData[i]);
+        this.pushToken(this.inputData[i]);
+        continue;
+      } else if (this.isQuote(this.inputData[i]) && this.isFullSatck()) {
+        this.stack.pop();
+      }
+
+      if (this.isSaperator(this.inputData[i]) && !this.isFullSatck()) {
+        this.pushToken(this.inputData[i]);
+      } else if (this.isSaperator(this.inputData[i - 1]) && this.isNotFisrt(i) && !this.isFullSatck()) {
+        this.pushToken(this.inputData[i]);
+      } else {
+        this.makeString(this.inputData[i]);
+      }
+    }
+    const tokenList = this.tokenArr.map((el) => el.trim());
+    const realTokenList = tokenList.filter((el) => el !== '' && el !== ',');
+    console.log(realTokenList);
+    return realTokenList;
+  }
+  pushToken(data) {
+    this.tokenArr.push(data);
+  }
+
+  makeString(data) {
+    return (this.tokenArr[this.tokenArr.length - 1] += data);
+  }
+
+  isSaperator(data) {
+    const saperatorArr = ['[', ']', ',', '{', '}', ':'];
+    return saperatorArr.includes(data);
+  }
+
+  isNotFisrt(idx) {
+    return idx >= 1;
+  }
+
+  isQuote(data) {
+    return data === '"';
+  }
+
+  isFullSatck() {
+    return this.stack.length !== 0;
+  }
+}
