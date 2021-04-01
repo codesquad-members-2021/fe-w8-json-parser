f9b5534efc14c67f2a9a2b9f5936c84ba7b740c9
Hongbi <dyongdi@gmail.com>
2021-04-01 18:05:43 +0900

    Chore: install nodemon, sass, style-loader, sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './public/JS/main.js',
+  entry: ['./public/JS/main.js', './public/SCSS/style.scss'],
   output: {
     path: path.resolve(__dirname, 'public/JS/dist'),
     filename: 'bundle.js',
@@ -16,7 +17,16 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
 };
