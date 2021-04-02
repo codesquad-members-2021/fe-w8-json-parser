20c6e6e7da2e859450368e684a7947b66ea6100d
Hongbi <dyongdi@gmail.com>
2021-04-02 14:48:34 +0900

    Feat: add render count event
    - 카운트한 것을 화면에 렌더링

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,6 +1,6 @@
 import { tokenize } from './tokenizer.js';
 import { lex } from './lexer.js';
-import { parse } from './parser.js';
+import { parse, countNumber, countString, countDepth } from './parser.js';
 import * as _ from './util.js';
 
 const $inputText = _.$('.input__text');
@@ -8,5 +8,9 @@ const $inputBtn = _.$('.input__btn');
 const $jsonTree = _.$('.json-tree');
 
 $inputBtn.addEventListener('click', () => {
-  $jsonTree.innerHTML = `<pre>${JSON.stringify(_.pipe(tokenize, lex, parse)($inputText.value), null, '  ')}</pre>`;
+  const lexedArr = _.pipe(tokenize, lex)($inputText.value)
+  $jsonTree.innerHTML = `<pre>${JSON.stringify(parse([...lexedArr]), null, '  ')}</pre>`;
+  $jsonTree.innerHTML += `<br> 배열 중첩 수준:${countDepth(lexedArr)}단계`
+  $jsonTree.innerHTML += `<br> 문자열 타입 갯수 ${countString(lexedArr)}개`
+  $jsonTree.innerHTML += `<br> 숫자 타입 갯수 ${countNumber(lexedArr)}개`
 })
