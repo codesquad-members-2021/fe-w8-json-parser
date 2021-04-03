b443f3a0c30aa3589c1e68a1efce74b6b039bf5a
juddroid <70361152+juddroid@users.noreply.github.com>
2021-04-03 22:08:29 +0900

    [Luccoon] 루쿤의 뜻깊은 마지막 PR (#43)
    * feat: parsing ..ing"
    
    * test
    
    * feat: 루쿤의 엄청 멋진 JSON Parser 완성
    
    Co-authored-by: reminderclock <reminderclock@naver.com>
    Co-authored-by: Juddroid <raccoon@Juddroidui-MacBookPro.local>

--- public/src/lexer.js
+++ public/src/lexer.js
@@ -2,17 +2,18 @@ export default class Lexer {
   constructor(tokenList) {
     this.tokenList = tokenList;
     this.lexerList = [];
+    this.stack = [];
   }
 
   getLexerList() {
-    this.tokenList.forEach((el) => this.pushList(this.getTokenElement(el)));
+    this.tokenList.forEach((el, idx) => this.pushList(this.getTokenElement(el, idx)));
     return this.lexerList;
   }
   pushList(el) {
     this.lexerList.push(el);
   }
 
-  getTokenElement(element) {
+  getTokenElement(element, index) {
     const elementObj = {};
     if (element === '[') {
       (elementObj.type = 'arrayOpen'), (elementObj.value = element);
@@ -22,12 +23,35 @@ export default class Lexer {
       (elementObj.type = 'arrayClose'), (elementObj.value = element);
       return elementObj;
     }
-    if (!isNaN(Number(element))) {
-      (elementObj.type = 'number'), (elementObj.value = element);
+    if (element === '{') {
+      (elementObj.type = 'objectOpen'), (elementObj.value = element);
       return elementObj;
     }
+    if (element === '}') {
+      (elementObj.type = 'objectClose'), (elementObj.value = element);
+      return elementObj;
+    }
+    if (element === ':') {
+      (elementObj.type = 'colon'), (elementObj.value = element);
+      return elementObj;
+    }
+    if (!isNaN(Number(element))) {
+      if (this.tokenList[index - 1] === ':') {
+        (elementObj.type = 'number'), (elementObj.propType = 'objectValue'), (elementObj.value = element);
+      } else {
+        (elementObj.type = 'number'), (elementObj.value = element);
+      }
+      return elementObj;
+    }
+
     if (typeof element === 'string') {
-      (elementObj.type = 'string'), (elementObj.value = element);
+      if (this.tokenList[index + 1] === ':') {
+        (elementObj.type = 'string'), (elementObj.propType = 'objectKey'), (elementObj.value = element);
+      } else if (this.tokenList[index - 1] === ':') {
+        (elementObj.type = 'string'), (elementObj.propType = 'objectValue'), (elementObj.value = element);
+      } else {
+        (elementObj.type = 'string'), (elementObj.value = element);
+      }
       return elementObj;
     }
     if (typeof element === 'boolean') {
--- public/src/parser.js
+++ public/src/parser.js
@@ -1,16 +1,40 @@
 export default class Parser {
   constructor(lexerList) {
     this.lexerList = lexerList;
-  }
 
-  parse(parentNode = { type: 'array', child: [] }) {
+    this.objectValue = {};
+  }
+  isObject(element) {
+    return element.type === 'objectOpen' ? 'object' : 'array';
+  }
+  parse(type) {
+    const parentNode = { type: type || `syntaxTree`, child: [] };
     while (this.lexerList.length > 0) {
       const element = this.lexerList.shift();
       if (element.type === 'arrayOpen') {
-        const childNode = this.parse();
+        const childNode = this.parse('array');
         parentNode.child.push(childNode);
       } else if (element.type === 'arrayClose') {
         break;
+      } else if (element.type === 'objectOpen') {
+        const childNode = this.parse('object');
+        console.log(childNode);
+        parentNode.child.push(childNode);
+      } else if (element.type === 'objectClose') {
+        break;
+      } else if (element.value === ':') continue;
+      else if (element.propType === 'objectKey') {
+        const propKey = {};
+        propKey.type = this.checkType(element.value);
+        propKey.value = element.value;
+        this.objectValue.propKey = propKey;
+      } else if (element.propType === 'objectValue') {
+        const propValue = {};
+        propValue.type = this.checkType(element.value);
+        propValue.value = element.value;
+        this.objectValue.propValue = propValue;
+
+        parentNode.child.push({ type: 'objectProperty', value: { ...this.objectValue } });
       } else {
         parentNode.child.push({ type: this.checkType(element.value), value: element.value });
       }
@@ -22,5 +46,6 @@ export default class Parser {
     if (value === null) return 'NULL';
     if (typeof value === 'boolean') return 'boolean';
     if (typeof value === 'string') return 'String';
+    if (typeof value === 'object') return 'objectProperty';
   }
 }
